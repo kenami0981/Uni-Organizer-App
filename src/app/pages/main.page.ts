dc1331c74f846223b82ada48abf82ded8af64efb
kenami0981 <amelia.janek.aj@gmail.com>
2026-02-07 12:47:47 +0100

    Data binding terms

--- src/app/pages/main.page.ts
+++ src/app/pages/main.page.ts
@@ -1,5 +1,17 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
-@Component({ selector: 'app-main-page', templateUrl: './main.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+  selector: 'app-main-page', 
+  templateUrl: './main.page.html', 
+  encapsulation: ViewEncapsulation.None, 
+  changeDetection: ChangeDetectionStrategy.OnPush })
 export class MainPage {
+  terms = [{id: "sem1", name: "Semestr 1"}, 
+    {id: "sem2", name: "Semestr 2"},
+    {id: "sem3", name: "Semestr 3"},
+    {id: "sem4", name: "Semestr 4"},
+    {id: "sem5", name: "Semestr 5"},
+    {id: "sem6", name: "Semestr 6"},
+    {id: "sem7", name: "Semestr 7"},
+  ]
 }
