d2cbda4057d663e0e2aa00b94ab2d06bd8a9e242
kenami0981 <amelia.janek.aj@gmail.com>
2026-03-06 20:11:30 +0100

    UI changes: background, title, termBtn

--- projects/lib/src/lib/adapters/primary/ui/term.component.ts
+++ projects/lib/src/lib/adapters/primary/ui/term.component.ts
@@ -2,10 +2,11 @@ import { Component, ViewEncapsulation, ChangeDetectionStrategy, inject } from '@
 import { FirebaseSemesterService } from "../../secondary/infrastructure/firebase-semester.service"
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({ 
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, ReactiveFormsModule],
     selector: 'lib-term', 
     templateUrl: './term.component.html', 
     encapsulation: ViewEncapsulation.None, 
@@ -13,10 +14,20 @@ import { RouterLink } from '@angular/router';
 export class TermComponent {
     private semesterService = inject(FirebaseSemesterService);
     semesters$ = this.semesterService.getSemesters();
+    semesterForm!: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+      this.semesterForm = this.fb.group({
+        semester: ['', Validators.required]
+      })
+    }
 
-  addSemester(value:string) {
+  addSemester() {
+    if (this.semesterForm.invalid) return;
+    let value  = this.semesterForm.value.semester;
     this.semesterService.addSemester(value);
+    this.changeVisibility(1)
+    this.semesterForm.reset();
   }
   editSemester(semesterID: string, newName: string) {
     
@@ -24,5 +35,13 @@ export class TermComponent {
   }
   deleteSemester(semesterID: string) {
     this.semesterService.deleteSemester(semesterID);
+  }
+  changeVisibility(num: number) {
+    let tab: string[] = ["hidden", "visible"]
+    var showAddSemesterBtn = document.getElementById("showAddSemesterBtn")
+    var addSemesterForm = document.getElementById("addSemesterForm")
+    showAddSemesterBtn!.style.visibility = tab[num]
+    addSemesterForm!.style.visibility = tab[(num+1)%2]
+    
   }
 }
